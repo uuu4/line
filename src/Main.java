e1e78c595e5d15ca2f3e8ba3cc8d5934477cfd5e
Ali Emre Aydın <23120205056@ismu.edu.tr>
2024-05-13 00:00:21 +0300

    main classina yeni satir ekledim
    sout sarumana selam

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
         SwingUtilities.invokeLater(() -> {
             GUI example = new GUI();
             example.setVisible(true);
+            System.out.printf("SARUMANA SELAM");    
         });
     }
 }
