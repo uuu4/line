6a6d518258e453f55414336dc6dbdff6c46f7858
Ali Emre Aydın <23120205056@ismu.edu.tr>
2024-05-14 23:57:13 +0300

    changes in GUI
    switched into null layout in GUI class

--- src/GUI.java
+++ src/GUI.java
@@ -3,55 +3,58 @@ import java.awt.*;
 
 public class GUI extends JFrame {
     public GUI(){
-        JFrame frame = new JFrame("github.com/uuu4");
-        frame.setSize(1200,1200);
+        // bilesen ekleme
+        JFrame frame = new JFrame();
+        JPanel panel = new JPanel();
+        JLabel foto1 = new JLabel("Image1");
+        JLabel foto2 = new JLabel("Image2");
+        JLabel foto3 = new JLabel("Image3");
+        JLabel foto4 = new JLabel("Image4");
+        JLabel foto5 = new JLabel("Image5");
+        JLabel foto6 = new JLabel("Image6");
+        JLabel foto7 = new JLabel("Image7");
+        JLabel foto8 = new JLabel("Image8");
+        JButton nasilKullanirim = new JButton("Nasıl Kullanılır?");
+
+        //sabit frame ayarlari
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        JPanel disPanel = new JPanel(new GridLayout(2, 1));
-        disPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-
-        JPanel icPanel = new JPanel(new GridLayout(1, 2));
-        JPanel icicPanel = new JPanel(new GridLayout(3,3));
-        JLabel label1 = new JLabel("(diyagram cizme yeri)");
-        JLabel label2 = new JLabel("(islem gecmisi)");
-        JLabel label12 = new JLabel("(kare)");
-        JLabel label4 = new JLabel("(ucgen)");
-        JLabel label5 = new JLabel("(dikdortgen)");
-        JLabel label6 = new JLabel("(daire)");
-        JLabel label7 = new JLabel("(osuruk)");
-        JLabel label8 = new JLabel("(ucgen)");
-        JLabel label9 = new JLabel("(daire)");
-        JLabel label10 = new JLabel("(osuruk)");
-        JLabel label11 = new JLabel("(ucgen)");
-
-        icPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-        icicPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-        label1.setHorizontalAlignment(SwingConstants.CENTER);
-        icPanel.add(label1);
-        label2.setHorizontalAlignment(SwingConstants.CENTER);
-        icPanel.add(label2);
-        disPanel.add(icPanel);
-        JLabel label3 = new JLabel("terminal");
-        label3.setHorizontalAlignment(SwingConstants.CENTER);
-        disPanel.add(label3);
-        icPanel.add(icicPanel);
-        icicPanel.add(label12);
-        icicPanel.add(label4);
-        icicPanel.add(label5);
-        icicPanel.add(label6);
-        icicPanel.add(label7);
-        icicPanel.add(label8);
-        icicPanel.add(label9);
-        icicPanel.add(label10);
-        icicPanel.add(label11);
-
-
-
-
-        frame.getContentPane().add(disPanel);
-
-
+        frame.setResizable(false);
+        frame.setSize(1200, 1200);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
+
+        //panel ayarlari
+        panel.setLayout(null);
+        frame.add(panel);
+
+        //label ayarlari
+        foto1.setBounds(25, 25, 150, 150);
+        panel.add(foto1);
+
+        foto2.setBounds(25+foto1.getWidth(),25,150,150);
+        panel.add(foto2);
+
+        foto3.setBounds(25,25+foto1.getHeight(),150,150);
+        panel.add(foto3);
+
+        foto4.setBounds(25+foto1.getWidth(),25+foto1.getHeight(),150,150);
+        panel.add(foto4);
+
+        foto5.setBounds(25,25+2*foto1.getHeight(),150,150);
+        panel.add(foto5);
+
+        foto6.setBounds(25+foto1.getWidth(),25+2*foto1.getHeight(),150,150);
+        panel.add(foto6);
+
+        foto7.setBounds(25,25+3*foto1.getHeight(),150,150);
+        panel.add(foto7);
+
+        foto8.setBounds(25+foto1.getWidth(),25+3*foto1.getHeight(),150,150);
+        panel.add(foto8);
+
+        //button ayarlari
+        nasilKullanirim.setBounds(frame.getWidth()-200,0,150,20);
+        panel.add(nasilKullanirim);
+
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,8 @@ import java.awt.*;
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            GUI example = new GUI();
-            example.setVisible(true);   
+            GUI gui = new GUI();
+            gui.setVisible(true);
         });
     }
 }
