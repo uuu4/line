419db92c49b7a282d420a9f8564635f168da99cd
emir-basak-sunar <150067969+emir-basak-sunar@users.noreply.github.com>
2024-05-22 19:55:04 +0300

    panel2de değişiklik
    panel2'de artık 7den fazla resim atılabiliyor ve panel1de resimin klonu oluşuyor

--- src/Panel2.java
+++ src/Panel2.java
@@ -3,18 +3,19 @@ import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionAdapter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Panel2 extends JPanel {
-    ImageIcon img1, img2, img3, img4, img5, img6, img7;
+    List<ImageIcon> images = new ArrayList<>();
+    List<Point> imageCorners = new ArrayList<>();
 
     final int IMG_WIDTH = 100; // Assuming fixed width for simplicity
     final int IMG_HEIGHT = 100; // Assuming fixed height for simplicity
 
-    Point img1Corner, img2Corner, img3Corner, img4Corner, img5Corner, img6Corner, img7Corner;
-
     Point prevPoint;
 
-    int selectedImage = 0; // 0: None, 1: img1, 2: img2, etc.
+    int selectedImageIndex = -1; // -1: None
 
     Panel2() {
         ClickListener clickListener = new ClickListener();
@@ -27,39 +28,29 @@ public class Panel2 extends JPanel {
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        if (img1 != null) img1.paintIcon(this, g, (int) img1Corner.getX(), (int) img1Corner.getY());
-        if (img2 != null) img2.paintIcon(this, g, (int) img2Corner.getX(), (int) img2Corner.getY());
-        if (img3 != null) img3.paintIcon(this, g, (int) img3Corner.getX(), (int) img3Corner.getY());
-        if (img4 != null) img4.paintIcon(this, g, (int) img4Corner.getX(), (int) img4Corner.getY());
-        if (img5 != null) img5.paintIcon(this, g, (int) img5Corner.getX(), (int) img5Corner.getY());
-        if (img6 != null) img6.paintIcon(this, g, (int) img6Corner.getX(), (int) img6Corner.getY());
-        if (img7 != null) img7.paintIcon(this, g, (int) img7Corner.getX(), (int) img7Corner.getY());
+        for (int i = 0; i < images.size(); i++) {
+            ImageIcon image = images.get(i);
+            Point corner = imageCorners.get(i);
+            image.paintIcon(this, g, (int) corner.getX(), (int) corner.getY());
+        }
     }
 
     private class ClickListener extends MouseAdapter {
         @Override
         public void mousePressed(MouseEvent event) {
             Point clickPoint = event.getPoint();
-
-            if (isWithinImage(clickPoint, img1Corner)) {
-                selectedImage = 1;
-            } else if (isWithinImage(clickPoint, img2Corner)) {
-                selectedImage = 2;
-            } else if (isWithinImage(clickPoint, img3Corner)) {
-                selectedImage = 3;
-            } else if (isWithinImage(clickPoint, img4Corner)) {
-                selectedImage = 4;
-            } else if (isWithinImage(clickPoint, img5Corner)) {
-                selectedImage = 5;
-            } else if (isWithinImage(clickPoint, img6Corner)) {
-                selectedImage = 6;
-            } else if (isWithinImage(clickPoint, img7Corner)) {
-                selectedImage = 7;
-            } else {
-                selectedImage = 0;
-            }
-
+            selectedImageIndex = getSelectedImageIndex(clickPoint);
             prevPoint = event.getPoint();
+        }
+
+        private int getSelectedImageIndex(Point clickPoint) {
+            for (int i = images.size() - 1; i >= 0; i--) {
+                Point imgCorner = imageCorners.get(i);
+                if (isWithinImage(clickPoint, imgCorner)) {
+                    return i;
+                }
+            }
+            return -1; // None selected
         }
 
         private boolean isWithinImage(Point clickPoint, Point imgCorner) {
@@ -71,53 +62,20 @@ public class Panel2 extends JPanel {
     private class DragListener extends MouseMotionAdapter {
         @Override
         public void mouseDragged(MouseEvent event) {
-            if (selectedImage != 0) {
+            if (selectedImageIndex != -1) {
                 Point currentPoint = event.getPoint();
-                Point imgCorner = getSelectedImageCorner();
+                Point imgCorner = imageCorners.get(selectedImageIndex);
                 imgCorner.translate((int) (currentPoint.getX() - prevPoint.getX()),
                         (int) (currentPoint.getY() - prevPoint.getY()));
                 prevPoint = currentPoint;
                 repaint();
             }
         }
-
-        private Point getSelectedImageCorner() {
-            switch (selectedImage) {
-                case 1: return img1Corner;
-                case 2: return img2Corner;
-                case 3: return img3Corner;
-                case 4: return img4Corner;
-                case 5: return img5Corner;
-                case 6: return img6Corner;
-                case 7: return img7Corner;
-                default: return null;
-            }
-        }
     }
 
     public void addImage(ImageIcon image, Point point) {
-        if (img1 == null) {
-            img1 = image;
-            img1Corner = point;
-        } else if (img2 == null) {
-            img2 = image;
-            img2Corner = point;
-        } else if (img3 == null) {
-            img3 = image;
-            img3Corner = point;
-        } else if (img4 == null) {
-            img4 = image;
-            img4Corner = point;
-        } else if (img5 == null) {
-            img5 = image;
-            img5Corner = point;
-        } else if (img6 == null) {
-            img6 = image;
-            img6Corner = point;
-        } else if (img7 == null) {
-            img7 = image;
-            img7Corner = point;
-        }
+        images.add(image);
+        imageCorners.add(point);
         repaint();
     }
 }
