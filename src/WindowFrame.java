2ac25e3b65c3f7ffc978edbe84daf2cc54eceaa2
emir-basak-sunar <150067969+emir-basak-sunar@users.noreply.github.com>
2024-05-25 19:14:26 +0300

    Update WindowFrame.java

--- src/WindowFrame.java
+++ src/WindowFrame.java
@@ -6,7 +6,7 @@ import java.awt.event.MouseEvent;
 public class WindowFrame extends JFrame {
     public WindowFrame() {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(500, 800);
+        this.setSize(1200, 800);
         this.setTitle("github.com/uuu4");
         this.setLocationRelativeTo(null);
         this.setResizable(true);
@@ -22,20 +22,23 @@ public class WindowFrame extends JFrame {
         Panel2 panel2 = new Panel2();
         panel2.setPreferredSize(new Dimension(900, 500)); // Set size of panel2
         panel2.setBorder(BorderFactory.createLineBorder(Color.BLACK, 5, false));
-        this.add(panel2, BorderLayout.CENTER); // Add panel2 to the right side
+        JScrollPane scrollPane2 = new JScrollPane(panel2);
+        scrollPane2.setBorder(BorderFactory.createLineBorder(Color.darkGray, 3, false));
+        scrollPane2.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        scrollPane2.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        this.add(scrollPane2, BorderLayout.CENTER);
 
         // Panel3 with JScrollPane
         Panel3 panel3 = new Panel3();
         panel3.setPreferredSize(new Dimension(1180, 300)); // Set preferred size larger to allow scrolling
-        panel3.setBackground(Color.darkGray);
+        panel3.setBackground(Color.WHITE);
 
-        JScrollPane scrollPane = new JScrollPane(panel3);
-        scrollPane.setBorder(BorderFactory.createLineBorder(Color.darkGray, 5, false)); // Test için sınır çizin
-        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        JScrollPane scrollPane3 = new JScrollPane(panel3);
+        scrollPane3.setBorder(BorderFactory.createLineBorder(Color.darkGray, 5, false));
+        scrollPane3.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        scrollPane3.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
 
-        // Adding scrollPane to the bottom of the frame
-        this.add(scrollPane, BorderLayout.SOUTH);
+        this.add(scrollPane3, BorderLayout.SOUTH);
 
         // Set relationships
         panel1.setPanel2(panel2);
