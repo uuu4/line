2fbbc1d00f60926f0bf2ec5146d2426d1425f987
uuu4 <71949472+uuu4@users.noreply.github.com>
2024-05-29 21:42:13 +0300

    Update Panel2.java

--- src/Panel2.java
+++ src/Panel2.java
@@ -1,20 +1,32 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseMotionAdapter;
+import java.awt.event.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Panel2 extends JPanel {
     List<ImageIcon> images = new ArrayList<>();
     List<Point> imageCorners = new ArrayList<>();
+    List<JTextField> inputs = new ArrayList<>();
+    List<String> imageNames = new ArrayList<>();
+    List<List<Integer>> connections = new ArrayList<>();
+
+    private Panel3 panel3;
 
     final int IMG_WIDTH = 100;
     final int IMG_HEIGHT = 100;
 
+    ImageIcon img1 = new ImageIcon(getClass().getResource("/images/1.png")); // Use getResource to load image from classpath
+    ImageIcon img2 = new ImageIcon(getClass().getResource("/images/2.png"));
+    ImageIcon img3 = new ImageIcon(getClass().getResource("/images/3.png"));
+    ImageIcon img4 = new ImageIcon(getClass().getResource("/images/4.png"));
+    ImageIcon img5 = new ImageIcon(getClass().getResource("/images/5.png"));
+    ImageIcon img6 = new ImageIcon(getClass().getResource("/images/6.png"));
+    ImageIcon img7 = new ImageIcon(getClass().getResource("/images/7.png"));
+
     Point prevPoint;
     int selectedImageIndex = -1;
+    int lastSelectedIndex = -1;
 
     Panel2() {
         ClickListener clickListener = new ClickListener();
@@ -22,6 +34,36 @@ public class Panel2 extends JPanel {
 
         DragListener dragListener = new DragListener();
         this.addMouseMotionListener(dragListener);
+
+        JButton runButton = new JButton("Run");
+        runButton.setBounds(10, 10, 80, 30);
+        runButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (panel3 != null) {
+                    panel3.addInputs(getInputs());
+                    panel3.addConnections(getConnections());
+                    panel3.updateCode();
+                    panel3.runCode();
+                }
+            }
+        });
+        this.setLayout(null);
+        this.add(runButton);
+
+        // Clear button
+        JButton clearButton = new JButton("Clear");
+        clearButton.setBounds(100, 10, 80, 30);  // Adjust the position and size as needed
+        clearButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                clearPanel();
+                if (panel3 != null) {
+                    panel3.clearPanel();
+                }
+            }
+        });
+        this.add(clearButton);
     }
 
     @Override
@@ -32,13 +74,17 @@ public class Panel2 extends JPanel {
             ImageIcon image = images.get(i);
             Point corner = imageCorners.get(i);
             image.paintIcon(this, g, corner.x, corner.y);
+            JTextField input = inputs.get(i);
+            if (input != null) {
+                input.setLocation(corner.x + IMG_WIDTH / 2 - input.getWidth() / 2, corner.y + IMG_HEIGHT + 5);
+            }
         }
         // Draw lines between images
         g.setColor(Color.BLACK);
-        for (int i = 1; i < imageCorners.size(); i++) {
-            Point prev = imageCorners.get(i - 1);
-            Point current = imageCorners.get(i);
-            g.drawLine(prev.x + IMG_WIDTH / 2, prev.y + IMG_HEIGHT / 2, current.x + IMG_WIDTH / 2, current.y + IMG_HEIGHT / 2);
+        for (List<Integer> connection : connections) {
+            Point p1 = imageCorners.get(connection.get(0));
+            Point p2 = imageCorners.get(connection.get(1));
+            g.drawLine(p1.x + IMG_WIDTH / 2, p1.y + IMG_HEIGHT / 2, p2.x + IMG_WIDTH / 2, p2.y + IMG_HEIGHT / 2);
         }
     }
 
@@ -47,6 +93,11 @@ public class Panel2 extends JPanel {
         public void mousePressed(MouseEvent event) {
             Point clickPoint = event.getPoint();
             selectedImageIndex = getSelectedImageIndex(clickPoint);
+            if (selectedImageIndex != -1 && lastSelectedIndex != -1 && lastSelectedIndex != selectedImageIndex) {
+                connections.add(List.of(lastSelectedIndex, selectedImageIndex));
+                repaint();
+            }
+            lastSelectedIndex = selectedImageIndex;
             prevPoint = event.getPoint();
         }
 
@@ -79,9 +130,52 @@ public class Panel2 extends JPanel {
         }
     }
 
-    public void addImage(ImageIcon image, Point point) {
+    public void addImage(ImageIcon image, Point point, boolean addTextField) {
         images.add(image);
         imageCorners.add(point);
+
+        if (addTextField) {
+            JTextField input = new JTextField(10);
+            input.setSize(IMG_WIDTH, 20);
+            inputs.add(input);
+            add(input);
+        } else {
+            inputs.add(null);
+        }
+
+        imageNames.add(image.getDescription()); // Add the image name to the list
+        repaint();
+    }
+
+    public List<JTextField> getInputs() {
+        return inputs;
+    }
+
+    public List<String> getImageNames() {
+        return imageNames;
+    }
+
+    public List<List<Integer>> getConnections() {
+        return connections;
+    }
+
+    public void setPanel3(Panel3 panel3) {
+        this.panel3 = panel3;
+    }
+
+    public void clearPanel() {
+        images.clear();
+        imageCorners.clear();
+        for (JTextField input : inputs) {
+            if (input != null) {
+                this.remove(input);
+            }
+        }
+        inputs.clear();
+        imageNames.clear();
+        connections.clear();
+        selectedImageIndex = -1;
+        lastSelectedIndex = -1;
         repaint();
     }
 }
