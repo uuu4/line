945510c65218ec59a8fe86273132dbdaaff3ff4d
emir-basak-sunar <150067969+emir-basak-sunar@users.noreply.github.com>
2024-05-29 16:31:40 +0300

    panel 3de güncelleme
    panel3de döngülerden sonraki ilk eklenen komut döngünün içine giriyor

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 import javax.swing.*;
 import java.awt.*;
-
 public class Main {
     public static void main(String[] args) {
         new WindowFrame();
--- src/Panel3.java
+++ src/Panel3.java
@@ -10,12 +10,12 @@ public class Panel3 extends JPanel {
     private JTextArea codeArea;
 
     Panel3() {
-        this.setLayout(new BorderLayout()); // BoxLayout yerine BorderLayout kullan
+        this.setLayout(new BorderLayout());
         codeArea = new JTextArea(20, 50);
         codeArea.setFont(new Font("Monospaced", Font.PLAIN, 12));
         codeArea.setEditable(false);
-        JScrollPane scrollPane = new JScrollPane(codeArea); // JTextArea'nın kendi JScrollPane'ini kullan
-        this.add(scrollPane, BorderLayout.CENTER); // JScrollPane'i BorderLayout'un CENTER bölgesine ekle
+        JScrollPane scrollPane = new JScrollPane(codeArea);
+        this.add(scrollPane, BorderLayout.CENTER);
     }
 
     public void addImageName(String imageName) {
@@ -41,70 +41,84 @@ public class Panel3 extends JPanel {
         sb.append("public class GeneratedCode {\n");
         sb.append("    public static void main(String[] args) {\n");
 
-        for (int i = 0; i < imageNames.size(); i++) {
-            String imageName = imageNames.get(i);
-            String code = getCodeForShape(imageName, i);
-            sb.append(code);
-        }
+        appendNestedCode(sb, -1, "    ");
 
         sb.append("    }\n");
         sb.append("}\n");
         return sb.toString();
     }
 
-    private String getCodeForShape(String shapeName, int index) {
+    private void appendNestedCode(StringBuilder sb, int parentIndex, String indent) {
+        for (int i = 0; i < imageNames.size(); i++) {
+            if (parentIndex == -1 || isConnected(parentIndex, i)) {
+                String imageName = imageNames.get(i);
+                String code = getCodeForShape(imageName, i, indent);
+                sb.append(code);
+                if (imageName.equals("IfBlock") || imageName.equals("ElseBlock") || imageName.equals("Loop")) {
+                    int nestedIndex = getFirstConnectedIndex(i);
+                    if (nestedIndex != -1) {
+                        String nestedCode = getCodeForShape(imageNames.get(nestedIndex), nestedIndex, indent + "    ");
+                        sb.append(nestedCode);
+                    }
+                    sb.append(indent).append("}\n");  // Close the block
+                }
+            }
+        }
+    }
+
+    private boolean isConnected(int parentIndex, int childIndex) {
+        for (List<Integer> connection : connections) {
+            if (connection.get(0) == parentIndex && connection.get(1) == childIndex) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private int getFirstConnectedIndex(int parentIndex) {
+        for (List<Integer> connection : connections) {
+            if (connection.get(0) == parentIndex) {
+                return connection.get(1);
+            }
+        }
+        return -1;
+    }
+
+    private String getCodeForShape(String shapeName, int index, String indent) {
         StringBuilder code = new StringBuilder();
         switch (shapeName) {
             case "Start":
-                code.append("        // Start\n");
-                code.append("        System.out.println(\"Start\");\n");
+                code.append(indent).append("// Start\n");
+                code.append(indent).append("System.out.println(\"Start\");\n");
                 break;
             case "Process":
-                code.append("        // Process\n");
-                code.append("        ");
-                code.append(inputs.get(index).getText()).append(";\n");
+                code.append(indent).append("// Process\n");
+                code.append(indent).append(inputs.get(index).getText()).append(";\n");
                 break;
             case "IfBlock":
-                code.append("        // IfBlock\n");
-                code.append("        if (").append(inputs.get(index).getText()).append(") {\n");
-                appendNestedCode(code, index, "        ");
-                code.append("        }\n");
+                code.append(indent).append("// IfBlock\n");
+                code.append(indent).append("if (").append(inputs.get(index).getText()).append(") {\n");
                 break;
             case "ElseBlock":
-                code.append("        // ElseBlock\n");
-                code.append("        else {\n");
-                appendNestedCode(code, index, "        ");
-                code.append("        }\n");
+                code.append(indent).append("// ElseBlock\n");
+                code.append(indent).append("else {\n");
                 break;
             case "Output":
-                code.append("        // Output\n");
-                code.append("        System.out.println(").append(inputs.get(index).getText()).append(");\n");
+                code.append(indent).append("// Output\n");
+                code.append(indent).append("System.out.println(").append(inputs.get(index).getText()).append(");\n");
                 break;
             case "Loop":
-                code.append("        // Loop\n");
-                code.append("        while (").append(inputs.get(index).getText()).append(") {\n");
-                appendNestedCode(code, index, "        ");
-                code.append("        }\n");
+                code.append(indent).append("// Loop\n");
+                code.append(indent).append("while (").append(inputs.get(index).getText()).append(") {\n");
                 break;
             case "End":
-                code.append("        // End\n");
-                code.append("        System.out.println(\"End\");\n");
+                code.append(indent).append("// End\n");
+                code.append(indent).append("System.out.println(\"End\");\n");
                 break;
             default:
-                code.append("        // Unknown shape\n");
+                code.append(indent).append("// Unknown shape\n");
                 break;
         }
         return code.toString();
-    }
-
-    private void appendNestedCode(StringBuilder code, int parentIndex, String indent) {
-        for (List<Integer> connection : connections) {
-            if (connection.get(0) == parentIndex) {
-                int childIndex = connection.get(1);
-                String shapeName = imageNames.get(childIndex);
-                String nestedCode = getCodeForShape(shapeName, childIndex).replace("        ", indent + "    ");
-                code.append(nestedCode);
-            }
-        }
     }
 }
