b8db3361eb235207a2a5a4f7280a07d1795d1410
uuu4 <71949472+uuu4@users.noreply.github.com>
2024-05-29 21:41:09 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,5 @@
 import javax.swing.*;
-import javax.swing.border.EmptyBorder;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+
 
 public class Main {
     public static void main(String[] args) {
